d017caaae8ed480d1d5515178ac34a73083d27e9
monieshe <elarak1234@gmail.com>
2025-08-04 22:02:58 +0530

    arr.c

---
+++ arr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int arr[7] = {10, 20, 30, 40, 50, 60, 70};
+
+    printf("Array elements:\n");
+    for (int i = 0; i <= 7; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
